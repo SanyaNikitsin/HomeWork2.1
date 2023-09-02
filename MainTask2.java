4917253b651180231a74555ed8ad92134593e89b
Sanya <sanyanikitsin69@gmail.com>
2023-09-02 07:53:44 +0200

    Задание 2

---
+++ MainTask2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MainTask2 {
+    public static void main(String[] args) {
+        int num;
+        System.out.println("Введите температуру: ");
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
+        if (num > -5)
+        {
+            System.out.println("тепло");
+        }
+        else if (-5 >= num && num > -5)//по улсовию -20 но тогда выполняется следующий блок если пользователь ввёл до -20
+        {
+            System.out.println("нормально");
+        } if (num <= -20) ;
+        {
+            System.out.println("Холодно");
+        }
+    }
+}
