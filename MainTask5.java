53baeececcd8e430f8ea8737c0765d5d0fab7334
Sanya <sanyanikitsin69@gmail.com>
2023-09-02 09:27:28 +0200

    Задание 4

---
+++ MainTask5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MainTask5 {
+    public static void main(String[] args) {
+        int num;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое положительное число");
+        try {
+            num = scanner.nextInt();
+        }
+    }
+}
