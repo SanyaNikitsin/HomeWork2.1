c0515c80d23136e7627c5b917f6f07d905f72e22
Sanya <sanyanikitsin69@gmail.com>
2023-09-02 08:37:04 +0200

    Задание 4

---
+++ mainTask4.java
@@ -0,0 +1,9 @@
+public class mainTask4 {
+    public static void main(String[] args) {
+        int x = 7;
+        while (x < 105) {
+            System.out.print(x + " ");
+            x += 7;//x=x+7
+        }
+    }
+}
