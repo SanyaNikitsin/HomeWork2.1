66495b2cd60411b179dda0352b3a7ce8c61bbe93
Sanya <sanyanikitsin69@gmail.com>
2023-10-15 13:10:20 +0200

    Home Work 12.1

--- HomeWork13/RomeoAndJuliet/src/HomeWork13/Main.java
+++ HomeWork13/RomeoAndJuliet/src/HomeWork13/Main.java
@@ -1,25 +1,38 @@
-package HomeWork13;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.Scanner;
+package HomeWork13;/*В задании приложен файл Ромео и Джульетта на английском, вычитать его в приложении.
+                   Проанализировать и записать в другой файл самое длинное слово*/
+
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args){
-        String inputFile = "C:\\Home Work\\HomeWork2.1\\HomeWork13\\romeo-and-juliet";
-        String outputFile = "C:\\Home Work\\HomeWork2.1\\HomeWork13\\output.txt";
-        try {
-            Scanner scanner = new Scanner(inputFile);
-            String longestWord = "";
-            while (scanner.hasNext()) {
-                String word = scanner.next();
-                if (word.length() > longestWord.length()) {
-                    longestWord = word;
-                }
+    public static void main(String[] args) throws IOException {
+        String text;
+        int max = 0;
+        String search = null;
+        try (FileReader fileReader = new FileReader("src\\HomeWork13\\Romeo and Juliet")) {
+            StringBuilder stringBuilder = new StringBuilder();
+            int i;
+            /*String line;*/
+            while ((i = fileReader.read()) != -1)
+                stringBuilder.append((char) i);
+            text = stringBuilder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] words = text.split("\\s|=|\n");
+        for (String t : words)
+            if (t.length() > max) {
+                max = t.length();
+                search = t;
             }
-            PrintWriter writer = new PrintWriter(outputFile);
-            writer.println("Самое длинное слово: " + longestWord);
-            writer.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Файл не найден: " + e.getMessage());
+        /*System.out.println(search);*/
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src\\HomeWork13\\File output"))){
+            writer.write(search);
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
         }
     }
 }
+
