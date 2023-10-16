37bb86a26a88f320a7b8f6aa54ab5633945b9a36
Sanya <sanyanikitsin69@gmail.com>
2023-10-16 14:54:08 +0200

    Home Work Collections

--- HomeWork13.2/HomeWork13.2/src/HomeWork13collections/Main.java
+++ HomeWork13.2/HomeWork13.2/src/HomeWork13collections/Main.java
@@ -1,9 +1,18 @@
 package HomeWork13collections;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter: ");
+        char[] line1 = sc.nextLine().toCharArray();
+        Set line2 = new HashSet();
+        for(char arr:line1)
+            if(Character.isDigit(arr))
+                line2.add(arr);
+        System.out.println(line2);
     }
 }
