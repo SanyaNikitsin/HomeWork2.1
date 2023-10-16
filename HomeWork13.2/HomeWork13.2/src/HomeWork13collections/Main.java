6625f7463800752d26ec3e5853b7ce2580a954cc
Sanya <sanyanikitsin69@gmail.com>
2023-10-16 14:49:29 +0200

    Home Work collections

---
+++ HomeWork13.2/HomeWork13.2/src/HomeWork13collections/Main.java
@@ -0,0 +1,9 @@
+package HomeWork13collections;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+    }
+}
